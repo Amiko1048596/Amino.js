09f5ccdf306b632619d3b18366e63ca222d45494
Amiko <158367628+Amiko1048596@users.noreply.github.com>
2024-04-19 16:12:34 +0300

    Add files via upload

--- aminojs/ws_client.js
+++ aminojs/ws_client.js
@@ -5,10 +5,10 @@ function sleep(s) {
     return new Promise(resolve => setTimeout(resolve, s * 1000));
 }
 
-class Callbacks{
-	constructor(debug=false){
-		this.debug=debug;
-		this.methods = {
+class Callbacks {
+    constructor(debug = false) {
+        this.debug = debug;
+        this.methods = {
             "304": this.#resolve_chat_action_start,
             "306": this.#resolve_chat_action_end,
             "1000": this.#resolve_chat_message,
@@ -72,76 +72,178 @@ class Callbacks{
         this.chat_actions_end = {
             "Typing": this.on_user_typing_end,
         };
-	}
-	#resolve_chat_message(self, data){
-        let key = `${data['o']['chatMessage']['type']}:${data['o']['chatMessage']['mediaType']??0}`;
-        return (self.chat_methods[key]??self.default)(data);
     }
-    #resolve_chat_action_start(self, data){
-        let key = data['o']['actions']??0;
-        return (self.chat_actions_start[key]??self.default)(data);
+    #resolve_chat_message(self, data) {
+        let key = `${data['o']['chatMessage']['type']}:${data['o']['chatMessage']['mediaType']??0}`;
+        return (self.chat_methods[key] ?? self.default)(data);
+    }
+    #resolve_chat_action_start(self, data) {
+        let key = data['o']['actions'] ?? 0;
+        return (self.chat_actions_start[key] ?? self.default)(data);
     }
 
-    #resolve_chat_action_end(self, data){
-        let key = data['o']['actions']??0;
-        return (self.chat_actions_end[key]??self.default)(data);
+    #resolve_chat_action_end(self, data) {
+        let key = data['o']['actions'] ?? 0;
+        return (self.chat_actions_end[key] ?? self.default)(data);
     }
-    resolve(data){
-    	data=JSON.parse(data);
-    	if (this.methods[data["t"]]) this.methods[data["t"]](this, data);
-    	else if (this.debug) console.log(JSON.stringify(data));
+    resolve(data) {
+        data = JSON.parse(data);
+        if (this.methods[data["t"]]) this.methods[data["t"]](this, data);
+        else if (this.debug) console.log(JSON.stringify(data));
     }
-    on_text_message(data, call_back=(data)=>data){call_back(data);}
-    on_image_message(data, call_back=(data)=>data){call_back(data);}
-    on_youtube_message(data, call_back=(data)=>data){call_back(data);}
-    on_strike_message(data, call_back=(data)=>data){call_back(data);}
-    on_voice_message(data, call_back=(data)=>data){call_back(data);}
-    on_sticker_message(data, call_back=(data)=>data){call_back(data);}
-    on_voice_chat_not_answered(data, call_back=(data)=>data){call_back(data);}
-    on_voice_chat_not_cancelled(data, call_back=(data)=>data){call_back(data);}
-    on_voice_chat_not_declined(data, call_back=(data)=>data){call_back(data);}
-    on_video_chat_not_answered(data, call_back=(data)=>data){call_back(data);}
-    on_video_chat_not_cancelled(data, call_back=(data)=>data){call_back(data);}
-    on_video_chat_not_declined(data, call_back=(data)=>data){call_back(data);}
-    on_avatar_chat_not_answered(data, call_back=(data)=>data){call_back(data);}
-    on_avatar_chat_not_cancelled(data, call_back=(data)=>data){call_back(data);}
-    on_avatar_chat_not_declined(data, call_back=(data)=>data){call_back(data);}
-    on_delete_message(data, call_back=(data)=>data){call_back(data);}
-    on_group_member_join(data, call_back=(data)=>data){call_back(data);}
-    on_group_member_leave(data, call_back=(data)=>data){call_back(data);}
-    on_chat_invite(data, call_back=(data)=>data){call_back(data);}
-    on_chat_background_changed(data, call_back=(data)=>data){call_back(data);}
-    on_chat_title_changed(data, call_back=(data)=>data){call_back(data);}
-    on_chat_icon_changed(data, call_back=(data)=>data){call_back(data);}
-    on_voice_chat_start(data, call_back=(data)=>data){call_back(data);}
-    on_video_chat_start(data, call_back=(data)=>data){call_back(data);}
-    on_avatar_chat_start(data, call_back=(data)=>data){call_back(data);}
-    on_voice_chat_end(data, call_back=(data)=>data){call_back(data);}
-    on_video_chat_end(data, call_back=(data)=>data){call_back(data);}
-    on_avatar_chat_end(data, call_back=(data)=>data){call_back(data);}
-    on_chat_content_changed(data, call_back=(data)=>data){call_back(data);}
-    on_screen_room_start(data, call_back=(data)=>data){call_back(data);}
-    on_screen_room_end(data, call_back=(data)=>data){call_back(data);}
-    on_chat_host_transfered(data, call_back=(data)=>data){call_back(data);}
-    on_text_message_force_removed(data, call_back=(data)=>data){call_back(data);}
-    on_chat_removed_message(data, call_back=(data)=>data){call_back(data);}
-    on_text_message_removed_by_admin(data, call_back=(data)=>data){call_back(data);}
-    on_chat_tip(data, call_back=(data)=>data){call_back(data);}
-    on_chat_pin_announcement(data, call_back=(data)=>data){call_back(data);}
-    on_voice_chat_permission_open_to_everyone(data, call_back=(data)=>data){call_back(data);}
-    on_voice_chat_permission_invited_and_requested(data, call_back=(data)=>data){call_back(data);}
-    on_voice_chat_permission_invite_only(data, call_back=(data)=>data){call_back(data);}
-    on_chat_view_only_enabled(data, call_back=(data)=>data){call_back(data);}
-    on_chat_view_only_disabled(data, call_back=(data)=>data){call_back(data);}
-    on_chat_unpin_announcement(data, call_back=(data)=>data){call_back(data);}
-    on_chat_tipping_enabled(data, call_back=(data)=>data){call_back(data);}
-    on_chat_tipping_disabled(data, call_back=(data)=>data){call_back(data);}
-    on_timestamp_message(data, call_back=(data)=>data){call_back(data);}
-    on_welcome_message(data, call_back=(data)=>data){call_back(data);}
-    on_invite_message(data, call_back=(data)=>data){call_back(data);}
-    on_user_typing_start(data, call_back=(data)=>data){call_back(data);}
-    on_user_typing_end(data, call_back=(data)=>data){call_back(data);}
-    default(data, call_back=(data)=>data){call_back(data);}
+    on_text_message(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_image_message(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_youtube_message(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_strike_message(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_voice_message(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_sticker_message(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_voice_chat_not_answered(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_voice_chat_not_cancelled(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_voice_chat_not_declined(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_video_chat_not_answered(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_video_chat_not_cancelled(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_video_chat_not_declined(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_avatar_chat_not_answered(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_avatar_chat_not_cancelled(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_avatar_chat_not_declined(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_delete_message(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_group_member_join(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_group_member_leave(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_chat_invite(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_chat_background_changed(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_chat_title_changed(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_chat_icon_changed(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_voice_chat_start(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_video_chat_start(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_avatar_chat_start(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_voice_chat_end(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_video_chat_end(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_avatar_chat_end(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_chat_content_changed(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_screen_room_start(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_screen_room_end(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_chat_host_transfered(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_text_message_force_removed(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_chat_removed_message(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_text_message_removed_by_admin(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_chat_tip(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_chat_pin_announcement(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_voice_chat_permission_open_to_everyone(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_voice_chat_permission_invited_and_requested(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_voice_chat_permission_invite_only(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_chat_view_only_enabled(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_chat_view_only_disabled(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_chat_unpin_announcement(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_chat_tipping_enabled(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_chat_tipping_disabled(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_timestamp_message(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_welcome_message(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_invite_message(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_user_typing_start(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    on_user_typing_end(data, call_back = (data) => data) {
+        call_back(data);
+    }
+    default (data, call_back = (data) => data) {
+        call_back(data);
+    }
 };
 
 class SocketHandler {
@@ -149,25 +251,25 @@ class SocketHandler {
         this.api = "wss://ws1.narvii.com";
         this.session = client.session;
         this.reconnectTime = 180;
-        this.debug=debug;
+        this.debug = debug;
     }
-    async start(callbacks=null){
-        this.callbacks=callbacks??(new Callbacks(this.debug));
-    	await this.reconnect_handler();
+    async start(callbacks = null) {
+        this.callbacks = callbacks ?? (new Callbacks(this.debug));
+        await this.reconnect_handler();
     }
     async reconnect_handler() {
-        let f=async function(self){
+        let f = async function(self) {
             while (true) {
-	            self.run_amino_socket();
-	            await sleep(self.reconnectTime);
-        	}
+                self.run_amino_socket();
+                await sleep(self.reconnectTime);
+            }
         }
         await f(this);
     }
     async handle_message(self, data) {
-    	data=String(data);
-    	if (this.debug) console.log(`[WS | Message], ${data}`);
-    	self.callbacks.resolve(data);
+        data = String(data);
+        if (this.debug) console.log(`[WS | Message], ${data}`);
+        self.callbacks.resolve(data);
     }
     async send(data) {
         this.ws.send(data);
@@ -207,10 +309,13 @@ class SocketHandler {
 
         this.ws.on("pong", (d) => {
             this.ws.pong("pong", true, (e) => {
-                if (this.debug) if (e) console.log(e);
+                if (this.debug)
+                    if (e) console.log(e);
             })
         })
-        this.ws.on('message', (data)=>{this.handle_message(this, data)});
+        this.ws.on('message', (data) => {
+            this.handle_message(this, data)
+        });
     }
     async close() {
         this.ws.terminate();
