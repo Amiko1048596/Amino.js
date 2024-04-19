a52980114dfbf7ebf189d7ea3c7f9bb67c8e3fb9
Amiko <158367628+Amiko1048596@users.noreply.github.com>
2024-04-19 19:43:25 +0300

    Add files via upload

--- aminojs/ws_client.js
+++ aminojs/ws_client.js
@@ -272,7 +272,7 @@ class SocketHandler {
         self.callbacks.resolve(data);
     }
     async send(data) {
-        this.ws.send(data);
+        this.ws.send(JSON.stringify(data));
     }
     async run_amino_socket() {
         let final = `${this.session.deviceId}|${Date.now()}`;
@@ -322,7 +322,349 @@ class SocketHandler {
     }
 };
 
+const Action = {
+    ACTION_BROWSING: "Browsing",
+    ACTION_CHATTING: "Chatting",
+    ACTION_COMMENTING: "Commenting",
+    ACTION_PLAYING: "Playing",
+    ACTION_POLLING: "Polling",
+    ACTION_RECORDING: "Recording",
+    ACTION_TYPING: "Typing",
+    ACTION_VOTING: "Voting",
+};
+
+const Channel = {
+    CHANNEL_TYPE_NONE: 0,
+    CHANNEL_TYPE_AUDIO: 1,
+    CHANNEL_TYPE_TEXT: 2,
+    CHANNEL_TYPE_AVATAR: 3,
+    CHANNEL_TYPE_VIDEO: 4,
+    CHANNEL_TYPE_SCREEN_ROOM: 5,
+};
+
+const Role = {
+    JOIN_ROLE_GUEST: 0,
+    JOIN_ROLE_PRESENTER: 1,
+    JOIN_ROLE_AUDIENCE: 2,
+    JOIN_ROLE_GUEST_AUDIENCE: 3,
+};
+
+const Message = {
+    COMMUNITY_DISABLED: 1,
+    ERROR_MESSAGE: 1,
+    CHAT_THREAD_ORGANIZER_LEFT: 2,
+    CHAT_THREAD_NOT_AVAILABLE: 3,
+    USER_PROFILE_BANNED: 4,
+    CHAT_THREAD_MEMBERSHIP_BANNED: 5,
+    CHAT_THREAD_NO_PRESENTER: 6,
+    CHAT_THREAD_PRIVATE_NOT_ACCEPT: 99,
+    JOIN_THREAD_CHANNEL_REQUEST: 100,
+    JOIN_THREAD_CHANNEL_RESPONSE: 101,
+    THREAD_CHANNEL_USER_LIST_MESSAGE: 102,
+    LEAVE_THREAD_CHANNEL_REQUEST: 103,
+    LEAVE_THREAD_CHANNEL_RESPONSE: 104,
+    FETCH_THREAD_CHANNEL_USER_LIST_REQUEST: 105,
+    THREAD_CHANNEL_USER_JOINED_MESSAGE: 106,
+    THREAD_CHANNEL_USER_LEFT_MESSAGE: 107,
+    UPDATE_THREAD_CHANNEL_REQUEST: 108,
+    UPDATE_THREAD_CHANNEL_RESPONSE: 109,
+    THREAD_STATUS_CHANGED_MESSAGE: 111,
+    UPDATE_USER_ROLE_REQUEST: 112,
+    UPDATE_USER_ROLE_RESPONSE: 113,
+    THREAD_CHANNEL_USER_STATUS_CHANGED_MESSAGE: 114,
+    THREAD_CHANNEL_FORCE_QUIT_MESSAGE: 115,
+    THREAD_CHANNEL_USER_PING_REQUEST: 116,
+    THREAD_CHANNEL_USER_PING_RESPONSE: 117,
+    MULTI_DEVICE_ERROR: 118,
+    SCREEN_RROM_PLAY_LIST_RESPONSE: 119,
+    UPDATE_PLAY_LIST_REQUEST: 120,
+    UPDATE_PLAY_LIST_RESPONSE: 121,
+    FETCH_PLAY_LIST_REQUEST: 122,
+    FORCE_UPDATE_USER_ROLE_REQ: 126,
+    FORCE_UPDATE_USER_ROLE_RESP: 127,
+    FORCE_UPDATE_USER_ROLE_MESSAGE: 128,
+    THREAD_WAIT_LIST_APPROVE_MESSAGE: 130,
+    THREAD_WAIT_LIST_CHANGED_MESSAGE: 131,
+    THREAD_WAIT_LIST_CLEAN_REQUEST: 132,
+    THREAD_WAIT_LIST_CLEAN_RESPONSE: 133,
+    THREAD_WAIT_LIST_JOIN_APPROVE_REQUEST: 134,
+    THREAD_WAIT_LIST_JOIN_APPROVE_RESPONSE: 135,
+    THREAD_WAIT_LIST_JOIN_CANCEL_REQUEST: 136,
+    THREAD_WAIT_LIST_JOIN_CANCEL_RESPENSE: 137,
+    THREAD_WAIT_LIST_JOIN_REQUEST: 138,
+    THREAD_WAIT_LIST_JOIN_RESPONSE: 139,
+    AGORA_TOKEN_REQUEST: 200,
+    AGORA_TOKEN_RESPONSE: 201,
+    SUBSCRIBE_LIVE_LAYER_REQUEST: 300,
+    SUBSCRIBE_LIVE_LAYER_RESPONSE: 301,
+    UNSUBSCRIBE_LIVE_LAYER_REQUEST: 302,
+    UNSUBSCRIBE_LIVE_LAYER_RESPONSE: 303,
+    REPORT_LIVE_LAYER_ACTIVE_REQUEST: 304,
+    REPORT_LIVE_LAYER_ACTIVE_RESPONSE: 305,
+    REPORT_LIVE_LAYER_INACTIVE_REQUEST: 306,
+    REPORT_LIVE_LAYER_INACTIVE_RESPONSE: 307,
+    LIVE_LAYER_USER_JOINED_EVENT: 400,
+    LIVE_LAYER_USER_LEFT_EVENT: 401,
+    CHAT_MESSAGE_DTO: 1000,
+    CHAT_MESSAGE_ACK_DTO: 1001,
+};
+
+class WSS {
+    constructor(client, socket, ndcId = 0, threadId = null, debug = false) {
+        if (Object.getPrototypeOf(ndcId).constructor.name == "String") ndcId = Number(ndcId);
+        this.ws=socket;
+        this.ndcId = ndcId;
+        this.threadId = threadId;
+        this.timestamp = () => Math.trunc(Number(`${Date.now()/1000}`.slice(3)) - 1000000);
+    }
+    async send(data){
+        await this.ws.send(data);
+    }
+    async send_remove_from_presenter(userId, joinRole = Role.JOIN_ROLE_AUDIENCE) {
+        let data = {
+            "o": {
+                "ndcId": this.ndcId,
+                "threadId": this.threadId,
+                "joinRole": joinRole,
+                "targetUid": userId,
+                "id": this.timestamp(),
+            },
+            "t": Message.FORCE_UPDATE_USER_ROLE_REQ,
+        };
+        await this.send(data);
+    }
+    async join_voice_chat(joinRole = Role.JOIN_ROLE_PRESENTER) {
+        let data = {
+            "o": {
+                "ndcId": this.ndcId,
+                "threadId": this.threadId,
+                "joinRole": joinRole,
+                "id": this.timestamp(),
+            },
+            "t": Message.UPDATE_USER_ROLE_REQUEST,
+        };
+        await this.send(data);
+    }
+    async join_video_chat(joinRole = Role.JOIN_ROLE_PRESENTER, channelType = Channel.CHANNEL_TYPE_SCREEN_ROOM) {
+        let data = {
+            "o": {
+                "ndcId": this.ndcId,
+                "threadId": this.threadId,
+                "joinRole": joinRole,
+                "channelType": channelType,
+                "id": this.timestamp(),
+            },
+            "t": Message.UPDATE_THREAD_CHANNEL_REQUEST,
+        };
+        await this.send(data);
+    }
+    async join_video_chat_as_viewer(joinRole = Role.JOIN_ROLE_AUDIENCE) {
+        let data = {
+            "o": {
+                "ndcId": this.ndcId,
+                "threadId": this.threadId,
+                "joinRole": joinRole,
+                "id": this.timestamp(),
+            },
+            "t": Message.UPDATE_USER_ROLE_REQUEST,
+        };
+        await this.send(data);
+    }
+    async run_vc(joinRole = Role.JOIN_ROLE_PRESENTER) {
+        let data = {
+            "o": {
+                "ndcId": this.ndcId,
+                "threadId": this.threadId,
+                "joinRole": joinRole,
+                "id": this.timestamp(),
+            },
+            "t": Message.UPDATE_USER_ROLE_REQUEST,
+        };
+        await this.send(data);
+    }
+    async start_vc(channelType = Channel.CHANNEL_TYPE_AUDIO) {
+        await this.run_vc();
+        let data = {
+            "o": {
+                "ndcId": this.ndcId,
+                "threadId": this.threadId,
+                "channelType": channelType,
+                "id": this.timestamp(),
+            },
+            "t": Message.UPDATE_THREAD_CHANNEL_REQUEST,
+        };
+        await this.send(data);
+    }
+    async end_vc(joinRole = Role.JOIN_ROLE_AUDIENCE) {
+        let data = {
+            "o": {
+                "ndcId": this.ndcId,
+                "threadId": this.threadId,
+                "joinRole": joinRole,
+                "id": this.timestamp(),
+            },
+            "t": Message.UPDATE_USER_ROLE_REQUEST,
+        };
+        await this.send(data);
+    }
+    async online() {
+        let target = `ndc://x${this.ndcId}/`;
+        let data = {
+            "o": {
+                "actions": [Action.ACTION_BROWSING],
+                "target": target,
+                "ndcId": this.ndcId,
+                "id": this.timestamp(),
+            },
+            "t": Message.REPORT_LIVE_LAYER_ACTIVE_REQUEST,
+        };
+        await this.send(data);
+    }
+    async browsing(blogId = null, blogType = 0) {
+        let target;
+        if (blogId && blogType) target = `ndc://x${this.ndcId}/blog/${blogId}`;
+        else target = `ndc://x${this.ndcId}/featured`;
+        let data = {
+            "o": {
+                "actions": [Action.ACTION_BROWSING],
+                "target": target,
+                "ndcId": this.ndcId,
+                "params": {
+                    "blogType": blogType
+                },
+                "id": this.timestamp(),
+            },
+            "t": Message.REPORT_LIVE_LAYER_INACTIVE_REQUEST,
+        };
+        await this.send(data);
+    }
+    async chatting(threadType = Channel.CHANNEL_TYPE_TEXT) {
+        let target = `ndc://x${this.ndcId}/chat-thread/${this.threadId}`;
+        let data = {
+            "o": {
+                "actions": [Action.ACTION_CHATTING],
+                "target": target,
+                "ndcId": this.ndcId,
+                "params": {
+                    "duration": 12800,
+                    "membershipStatus": 1,
+                    "threadType": threadType,
+                },
+                "id": this.timestamp(),
+            },
+            "t": Message.REPORT_LIVE_LAYER_INACTIVE_REQUEST,
+        };
+        await this.send(data);
+    }
+    async public_chats() {
+        let target = `ndc://x${this.ndcId}/public-chats`;
+        let data = {
+            "o": {
+                "actions": [Action.ACTION_BROWSING],
+                "target": target,
+                "ndcId": this.ndcId,
+                "params": {
+                    "duration": 859
+                },
+                "id": this.timestamp(),
+            },
+            "t": Message.REPORT_LIVE_LAYER_INACTIVE_REQUEST,
+        };
+        await this.send(data);
+    }
+    async leader_boards() {
+        let target = `ndc://x${this.ndcId}/leaderboards`;
+        let data = {
+            "o": {
+                "actions": [Action.ACTION_BROWSING],
+                "target": target,
+                "ndcId": this.ndcId,
+                "params": {
+                    "duration": 859
+                },
+                "id": this.timestamp(),
+            },
+            "t": Message.REPORT_LIVE_LAYER_INACTIVE_REQUEST,
+        };
+        await this.send(data);
+    }
+    async custom(actions, target, params) {
+        let data = {
+            "o": {
+                "actions": actions,
+                "target": target,
+                "ndcId": this.ndcId,
+                "params": params,
+                "id": this.timestamp(),
+            },
+            "t": Message.REPORT_LIVE_LAYER_INACTIVE_REQUEST,
+        };
+        await this.send(data);
+    }
+    async thread_join(joinRole = Role.JOIN_ROLE_PRESENTER) {
+        let data = {
+            "o": {
+                "ndcId": this.ndcId,
+                "threadId": this.threadId,
+                "joinRole": joinRole,
+                "id": this.timestamp(),
+            },
+            "t": Message.UPDATE_USER_ROLE_REQUEST,
+        };
+        await this.send(data);
+    }
+    async channel_join(channelType = Channel.CHANNEL_TYPE_SCREEN_ROOM) {
+        let data = {
+            "o": {
+                "ndcId": this.ndcId,
+                "threadId": this.threadId,
+                "channelType": channelType,
+                "id": this.timestamp(),
+            },
+            "t": Message.UPDATE_THREAD_CHANNEL_REQUEST,
+        };
+        await this.send(data);
+    }
+    async video_player(path = null, title = null, background = null, duration = 60000) {
+        await this.chatting();
+        await this.thread_join();
+        await this.channel_join();
+        let data = {
+            "o": {
+                "ndcId": this.ndcId,
+                "threadId": this.threadId,
+                "playlist": {
+                    "currentItemIndex": 0,
+                    "currentItemStatus": 1,
+                    "items": [{
+                        "author": null,
+                        "duration": duration,
+                        "isDone": false,
+                        "mediaList": [
+                            [100, background, null]
+                        ],
+                        "title": title,
+                        "type": 1,
+                        "url": `file://${path}`
+                    }]
+                },
+                "id": this.timestamp(),
+            },
+            "t": Message.UPDATE_PLAY_LIST_REQUEST,
+        };
+        await this.send(data);
+        data["o"]["playlist"]["currentItemStatus"] = 2;
+        data["o"]["playlist"]["items"][0]["isDone"] = true;
+        await this.send(data);
+    }
+}
+
 module.exports = {
     SocketHandler,
-    Callbacks
+    Callbacks,
+    Action,
+    Channel,
+    Role,
+    Message,
+    WSS,
 };
